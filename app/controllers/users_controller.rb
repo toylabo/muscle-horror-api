5166fcf64a864a5ef460538d0f9570cc9da65911
Taisei Nakajima <ne300085@senshu-u.jp>
2019-10-14 10:39:04 +0900

    create login logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,9 @@ class UsersController < ApplicationController
   def index
     login_user = User.find_by(name: params[:name], password: params[:password])
     if login_user
-      render json: { responseCode: 200, token }
+      render json: { responseCode: 200, token: login_user.token }, status: 200
     else
-
+      render json: {responseCode: 401, message: "invalid name or password."}, status: 401
     end
   end
 end
