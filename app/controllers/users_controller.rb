32ce5f4adf0a061a8778de4527257fd620de2a83
Taisei Nakajima <ne300085@senshu-u.jp>
2019-10-14 10:27:19 +0900

    has_secure_token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+  def index
+    login_user = User.find_by(name: params[:name], password: params[:password])
+    if login_user
+      render json: { responseCode: 200, token }
+    else
+
+    end
+  end
 end
