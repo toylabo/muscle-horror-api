506d74efadd78b873734400cf17f38ce41cb7939
Taisei Nakajima <ne300085@senshu-u.jp>
2019-10-14 10:00:17 +0900

    create user (model, controller)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_13_211439) do
+ActiveRecord::Schema.define(version: 2019_10_14_005623) do
 
   create_table "results", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2019_10_13_211439) do
     t.integer "affinity"
     t.integer "exploratory"
     t.integer "life"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
