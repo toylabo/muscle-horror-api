cd8cc4938978047b5d37623f1cbea65ab6c29797
Taisei Nakajima <ne300085@senshu-u.jp>
2019-10-14 10:47:21 +0900

    add_token_column_to_users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_14_005623) do
+ActiveRecord::Schema.define(version: 2019_10_14_014436) do
 
   create_table "results", force: :cascade do |t|
     t.string "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2019_10_14_005623) do
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "token"
   end
 
 end
