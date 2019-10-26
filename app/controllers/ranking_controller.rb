59f4c130095e7d238e3b52b0cbb5c1e8b8c54b90
Taisei Nakajima <ne300085@senshu-u.jp>
2019-10-27 00:06:21 +0900

    create ranking_controller

---
+++ app/controllers/ranking_controller.rb
@@ -0,0 +1,13 @@
+class RankingController < ApplicationController
+
+  def index
+    per_page = params[:per_page]&.to_i || 5
+    @page = params[:page]&.to_i || 1
+    (params[:order]&.downcase == 'asc') ? order = "ASC" : order = "DESC"
+    @results = Result.page(@page).per(per_page).order("score " + order)
+    @response_code = 200
+    @message = "Success!"
+    render 'index', formats: :json, handlers: :jbuilder, status: @response_code
+  end
+
+end
