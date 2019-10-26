04f10d366182edacd95d5f5d3e78596fdd8dce61
Taisei Nakajima <ne300085@senshu-u.jp>
2019-10-27 00:16:20 +0900

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :results, only: [:index, :create, :show, :destroy]
   post "/login", to: 'users#create'
+  get "/ranking", to: "ranking#index"
 end
